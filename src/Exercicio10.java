7d679f2fffb8bca14211cbcaba1df9dc527360c7
Matheus Luiz Bonatto <matheuslbonatto@gmail.com>
2024-04-17 20:28:06 -0300

    Fializado os exercicios

---
+++ src/Exercicio10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        /*Crie um programa que recebe o salário de um funcionário 
+          e verifica se ele está acima do salário mínimo atual */
+    double salario;
+    double salarioBase = 1412.00;
+
+    System.out.printf("Informe seu salário: ");
+    salario = scanner.nextDouble();
+
+if (salario > salarioBase) {
+
+    System.out.println("Você está ganhando mais que o salario base, parabéns!");
+    
+} 
+else {
+    System.out.println("Vish, está na hora de pedir um aumento salarial :/");
+}
+
+scanner.close();
+}
+    
+}
---
+++ src/Exercicio8.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    /*Desenvolva um programa que pede ao usuário para digitar um ano 
+    e verifica se é bissexto ou não. 
+    Um ano é bissexto se for divisível por 4, mas não por 100, a menos que também seja divisível por 400. */
+    Integer ano;
+    
+    System.out.println("Verificador de Ano Bissexto!");
+    System.out.printf("Digite um ano: ");
+    ano = scanner.nextInt();
+    
+    if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+     
+        System.out.println(ano + " É um ano bissexto.");
+    } else {
+        System.out.println(ano + " Não é um ano bissexto.");
+    }
+
+    scanner.close();
+
+    }
+    
+}
